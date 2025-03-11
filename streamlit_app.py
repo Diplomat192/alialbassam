4d98c1005c55d4980e8ffe4451248388418aa410
Diplomat192 <48728943+Diplomat192@users.noreply.github.com>
2025-03-11 06:09:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import folium
+import sklearn
 from streamlit_folium import folium_static
 import plotly.express as px
 from sklearn.ensemble import IsolationForest
